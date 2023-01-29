f0348731667d5e20c5d89980043caa2a0bc25cae
brunonegri <b.negri_@hotmail.com>
2023-01-29 00:12:22 -0300

    req 10 e 11 feito

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+
+    with open(path) as file:
+        data = file.read()
+        expected = data.lower().count("python")
+
+        assert expected == count_ocurrences(path, "python")
