e25563253b1ae60a73d1220bdae905b767ddace0
brunonegri <b.negri_@hotmail.com>
2023-01-28 23:45:53 -0300

    req 8 e 9 feito

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -30,46 +30,28 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        salary = int(salary)
+    except (ValueError, TypeError, KeyError):
+        raise ValueError
+    if min_salary > max_salary:
+        raise ValueError
+    return min_salary <= salary <= max_salary
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    filtered_jobs = []
+    for job in jobs:
+        try:
+            matches_salary = matches_salary_range(job, salary)
+            if matches_salary is True:
+                filtered_jobs.append(job)
+        except ValueError:
+            print("job not found")
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    return filtered_jobs
